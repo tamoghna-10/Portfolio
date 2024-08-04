f4b4298fbf8ae898a70aeba7e019182f12d0d481
tamoghna-10 <tms.cns45@gmail.com>
2024-08-04 06:48:46 +0530

    portfolio completed

---
+++ my-portfolio/src/components/Contact.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { CiFacebook } from "react-icons/ci";
+import { CiLinkedin } from "react-icons/ci";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaGithubSquare } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
+const Contact = () => {
+  return (
+    <>
+      <div className="container contact" id="contact">
+        <h1>Contact Me</h1>
+        <div className="contact-icon" data-aos="zoom-in-up" data-aos-duration="1000">
+          <a href="https://www.google.com" target="_blank" className="items">
+            <FaInstagram className="icons" />
+          </a>
+          <a href="https://www.facebook.com" target="_blank" className="items">
+            <CiFacebook className="icons" />
+          </a>
+          <a href="https://www.linkedin.com" target="_blank" className="items">
+            <CiLinkedin className="icons" />
+          </a>
+          <a href="https://www.google.com" target="_blank" className="items">
+            <FaSquareXTwitter className="icons" />
+          </a>
+          <a href="https://github.com" target="_blank" className="items">
+            <FaGithubSquare className="icons" />
+          </a>
+          <a href="mailto: tamoghnasanyal8@gmail.com" target="_blank"  className="items">
+            <SiGmail className="icons" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
